a3ebc3d22d8d0b212d1869a8304b8ac9574634a5
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-03-18 09:54:01 +0200

    Create Insertion_Sort.py

---
+++ Project1/Insertion_Sort.py
@@ -0,0 +1,29 @@
+#Insertion Sort
+import time
+start_time = time.time()
+f = open("date.in","r")
+g = open("date.out","w")
+linie = f.read().split()
+arr = [int(x) for x in linie] * 50
+arr2 = sorted(arr)
+
+
+for i in range(1,len(arr)):
+    key = arr[i]
+    j = i-1
+    while j>=0 and key < arr[j]:
+        arr[j+1] = arr[j]
+        j-=1
+    arr[j+1] = key
+
+
+
+if arr2 == arr:
+    for i in range(len(arr)):
+        g.write(f"{str(arr[i])} ")
+else:
+    print("Ceva gresit")
+
+print("--- %s seconds ---" % (time.time() - start_time))
+f.close()
+g.close()
