6c5944ee5777714cf4b980fa65cc40e3988df2f6
Dobrin Ionut <91891857+ionutdd@users.noreply.github.com>
2023-03-18 19:58:37 +0200

    Update predefined_function.py

--- Project1/predefined_function.py
+++ Project1/predefined_function.py
@@ -5,7 +5,7 @@ start_time = time.time()
 f = open("date.in","r")
 g = open("date.out","w")
 linie = f.read().split()
-arr = [int(x) for x in linie] * 1000
+arr = [int(x) for x in linie]
 
 arr.sort()
 for i in range(len(arr)):
